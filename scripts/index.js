80e9486b3c5e106852a039edb185ab68a4f957cd
TRYSTATE.FM <trystate.fm@Jasons-MBP.lan>
2025-12-10 21:20:09 -0600

    Add modal transitions, submit Edit Profile, log New Post inputs

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,8 @@
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+const editProfileBtn = document.querySelector(".profile__edit-btn");
+const newPostBtn = document.querySelector(".profile__new-btn");
+
 const initialCards = [
   {
     name: "Val Thorens",
@@ -30,20 +35,37 @@ const initialCards = [
   },
 ];
 
-const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileFormEl = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
 
-const newPostBtn = document.querySelector(".profile__new-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+const newPostFormEl = newPostModal.querySelector(".modal__form");
+const newPostLinkInput = newPostModal.querySelector("#image-link-input");
+const newPostCaptionInput = newPostModal.querySelector("#caption-input");
 
 editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
   editProfileModal.classList.add("modal_is-opened");
 });
 editProfileCloseBtn.addEventListener("click", function () {
   editProfileModal.classList.remove("modal_is-opened");
 });
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  editProfileModal.classList.remove("modal_is-opened");
+}
+editProfileFormEl.addEventListener("submit", handleProfileFormSubmit);
 
 newPostBtn.addEventListener("click", function () {
   newPostModal.classList.add("modal_is-opened");
@@ -51,6 +73,13 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+function handlePostFormSubmit(evt) {
+  evt.preventDefault();
+  console.log(newPostLinkInput.value)
+  console.log(newPostCaptionInput.value)
+  newPostModal.classList.remove("modal_is-opened");
+}
+newPostFormEl.addEventListener("submit", handlePostFormSubmit);
 
 initialCards.forEach((card) => {
   console.log(card.name);
